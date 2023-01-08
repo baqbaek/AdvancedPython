4b442d8bc6ea63aa0563bd346f6215a51a179a9f
Bartek <baqbaek@gmail.com>
2023-01-09 00:06:04 +0100

    lambda and comprehensions exercise

--- exercise2.py
+++ exercise2.py
@@ -6,4 +6,11 @@ print(list(map(lambda x: x ** x, my_list)))
 a = [(0, 2), (5, 2), (9, 9), (10, -1)]
 a.sort(key=lambda x: x[1])
 print(a)
+# 3 Comprehensions
+second_list = [x for x in 'Bartek']
+print(second_list)
+# 4 Find duplicates using only comprehensions
 
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+duplicates = list(set([x for x in some_list if some_list.count(x) > 1]))
+print(duplicates)
