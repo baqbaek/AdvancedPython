4ed886cdadd79e7e07d8bb702e3a947af156892b
Bartek <baqbaek@gmail.com>
2023-01-08 23:20:57 +0100

    map, filter, zip, reduce

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-def multipy(my_list):
-    new_list = []
-    for i in my_list:
-        new_list.append(i * 2)
-    return new_list
+# map, filer, zip, reduce
+from functools import reduce
+
+my_list = [1, 2, 3]
+second_list = [10, 20, 30]
+third_list = [5, 4, 3]
 
 
-print(multipy([1, 2, 3]))
+def multipy(x):
+    return x * 2
+
+
+def check_odd(x):
+    return x % 2 != 0
+
+
+def accumulator(acc, x):
+    return acc + x
+
+
+print(list(map(multipy, my_list)))  # map -> [2, 4, 6]
+print(list(filter(check_odd, my_list)))  # filter -> [1, 3]
+print(list(zip(my_list, second_list, third_list)))  # zip -> [(1, 10, 5), (2, 20, 4), (3, 30, 3)]
+print((reduce(accumulator, my_list, 0)))  # reduce -> 6
